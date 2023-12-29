cba4b4eaaf9443f8d9919420b6f6d388f5f9f4dc
Ranjith630364 <run.jeetkumar01@gmail.com>
2023-12-30 00:09:18 +0530

    code added for keycloak

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-using MeganUploadFiles.Authentication;
 using MeganUploadFiles.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.OpenApi.Models;
 
@@ -8,9 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddSingleton<IStorageService, StorageService>();
-//builder.Services.AddSingleton<IAppConfiguration, AppConfiguration>();
-builder.Services.ConfigureJWT(true, "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp3CVbF+TZDrO6IjeFIsbWhi4vjxkLrPy2ygWBbXse7ycd2daJrFXzKmlWECrSw7wbBcv4KO0PFVtb9s5PCJGmDZOZR02xY8DgUauU+S1EWEjmEdjeC8puEOPoM/YauNWWqAtJt8146WNTN07/UsvH7YZogKcmrl7PgmEMQtHhHYCWuoe+/27Mm4UffaIWIWrdVw970SFQ2PPAYns8j9NxWHMXUQZFewEKn1mw7RrByiP99PIOMoS7GtvnkWSls9VEXrsYelVjTEaXKiaKHYX6trzP4Zg9odOTMlULmeRLYs4lVCBC/lAg/BzM+3h/3I/1xLWy9S0sfwj3YCTboBL5wIDAQAB");
+// Key cloak Identity code -- start
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "http://localhost:8080/realms/myrealm";
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                };
+                options.RequireHttpsMetadata = false;
+            });
+
 builder.Services.AddAuthorization();
+// Key cloak Identity code -- end
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +30,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen((c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyWebApi", Version = "v1" });
+    
+    // Key cloak Identity code -- start
     //First we define the security scheme
     c.AddSecurityDefinition("Bearer", //Name the security scheme
         new OpenApiSecurityScheme
@@ -37,6 +50,7 @@ builder.Services.AddSwaggerGen((c =>
                         },new List<string>()
                     }
                 });
+    // Key cloak Identity code -- end
 }));
 
 
